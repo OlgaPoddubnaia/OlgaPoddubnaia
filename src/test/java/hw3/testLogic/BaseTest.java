19da011fe8e93fa20d3e1cd72d2d9dafce30dc00
Olga Poddubnaia <poddubnaiaolga@gmail.com>
2020-12-17 00:00:56 +0300

    убрала код из папки main->java, прописала LoginPAge,тест

--- src/test/java/hw3/testLogic/BaseTest.java
+++ src/test/java/hw3/testLogic/BaseTest.java
@@ -1,4 +1,37 @@
 package hw3.testLogic;
 
-public class BaseTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest extends Properties{
+    protected WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\SeleniumDrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        driver.quit();
+        driver = null;
+    }
+    protected static WebElement waitForElementLocatedBy(WebDriver driver, By by) {
+        WebElement searchInput = new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.presenceOfElementLocated(by));
+        return searchInput;
+    }
 }
