6f5cff09beaff164ebf9c4bfc0ee42be6e04b4d1
Olga Poddubnaia <poddubnaiaolga@gmail.com>
2020-12-18 13:04:59 +0300

    емного изменила названия. делаю комит, чтобы переключиться на другую ветку

---
+++ src/test/java/hw3/pageObjects/DifferentElementsPageAbstractPO.java
@@ -0,0 +1,12 @@
+package hw3.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class DifferentElementsPageAbstractPO extends AbstractPage {
+
+    public DifferentElementsPageAbstractPO(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
