ed2cf85e5916d3a242a913329427de80c69ab10a
OlgaPoddubnaia <kokmop-2Pygda-guwfib>
2021-01-19 20:47:47 +0300

    создала форму для заполнения

---
+++ src/main/java/hw6/forms/MetalsAndColorsForm.java
@@ -0,0 +1,13 @@
+package hw6.forms;
+
+import com.epam.jdi.light.elements.composite.Form;
+import hw6.entities.MetalsAndColorsFormDataFromJson;
+
+public class MetalsAndColorsForm extends Form<MetalsAndColorsFormDataFromJson> {
+
+
+
+
+
+
+}
